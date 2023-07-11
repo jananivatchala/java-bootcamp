450da2baa2b27e672dcbca6b1b9776566fbf31d3
jananivatchala <126151861+jananivatchala@users.noreply.github.com>
2023-07-11 07:13:10 -0700

    Add files via upload

---
+++ sum of five.java/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]) {
+            int i,n=0,s=0;
+            {
+
+                System.out.println("Input the 5 numbers : ");
+
+            }
+            for (i=0;i<5;i++)
+            {
+                Scanner in = new Scanner(System.in);
+                n = in.nextInt();
+
+                s +=n;
+            }
+        System.out.println("The sum of 5 no is : " +s);
+        }
+
+    }
